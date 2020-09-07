4d821a8115b4a5a980dce953d4adc8555e6366ed
GabrielaSmolen <gabrysiasmolen@gmail.com>
2020-09-07 14:08:58 +0200

    qrs extractor to csv function

--- qrs_extractor_to_csv.py
+++ qrs_extractor_to_csv.py
@@ -1,26 +1,36 @@
 import pandas as pd
-
+from os import listdir
+from os.path import isfile, join
 import wfdb
 
 
-if __name__ == '__main__':
+def qrs_extractor_to_csv(signal_path):
+    """doc"""
+    signal = wfdb.rdrecord(signal_path)
 
-    signal = wfdb.rdrecord('data/mit-bih-arrhythmia-database-1.0.0/100')
-
-    annotation = wfdb.rdann('data/mit-bih-arrhythmia-database-1.0.0/100', 'atr')
+    annotation = wfdb.rdann(signal_path, 'atr')
 
     samples = annotation.sample
 
     sampling_frq = signal.fs
 
-    window_len = 150*sampling_frq/1000
+    window_len = 150 * sampling_frq / 1000
 
     qrs_list = []
 
     for sample in samples:
-        qrs = signal.p_signal[int(sample-window_len):int(sample+window_len), 0]
+        qrs = signal.p_signal[int(sample - window_len):int(sample + window_len), 0]
         qrs_list.append(qrs.tolist())
 
     extracted_qrs = pd.DataFrame(qrs_list)
-    extracted_qrs.to_csv('data/extracted_qrs.csv', index=False, header=False)
+    extracted_qrs.to_csv(signal_path+'.csv', index=False, header=False)
 
+
+if __name__ == '__main__':
+    root = 'data/mit-bih-arrhythmia-database-1.0.0'
+
+    files = [file[0:3] for file in listdir(root) if isfile(join(root, file))]
+
+    for file in files:
+        print(file)
+        qrs_extractor_to_csv(join(root, file))
