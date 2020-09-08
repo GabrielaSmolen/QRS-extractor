a64ddcde9ce2bc7ea9fe5b6d425859559ed60a36
GabrielaSmolen <gabrysiasmolen@gmail.com>
2020-09-08 13:27:34 +0200

    refactor'

--- qrs_extractor_to_csv.py
+++ qrs_extractor_to_csv.py
@@ -7,32 +7,36 @@ import wfdb
 def qrs_extractor_to_csv(signal_path):
     """doc"""
     signal = wfdb.rdrecord(signal_path)
-
     annotation = wfdb.rdann(signal_path, 'atr')
 
     samples = annotation.sample
+    symbols = annotation.symbol
 
     sampling_frq = signal.fs
-
     window_len = 150 * sampling_frq / 1000
 
-    qrs_list = []
+    normal_qrs = []
+    abnormal_qrs = []
 
-    for sample in samples:
+    for sample, symbol in zip(samples,symbols):
         qrs = signal.p_signal[int(sample - window_len):int(sample + window_len), 0]
-        qrs_list.append(qrs.tolist())
+        if symbol in ['N', 'R', 'L', 'j', 'e']:
+            normal_qrs.append(qrs.tolist())
+        if symbol == 'V':
+            abnormal_qrs.append(qrs.tolist())
 
-    # qrs_list = list(set(tuple(qrs_list)))
+    extracted_qrs = pd.DataFrame(normal_qrs)
+    extracted_qrs.to_csv(signal_path+'_normal.csv', index=False, header=False)
 
-    extracted_qrs = pd.DataFrame(qrs_list)
-    extracted_qrs.to_csv(signal_path+'.csv', index=False, header=False)
+    extracted_qrs = pd.DataFrame(abnormal_qrs)
+    extracted_qrs.to_csv(signal_path + '_abnormal.csv', index=False, header=False)
 
 
 if __name__ == '__main__':
+
     root = 'data/mit-bih-arrhythmia-database-1.0.0'
 
     files = [file[0:3] for file in listdir(root) if isfile(join(root, file))]
-
     files = sorted(list(set(files)))
 
     for file in files:
