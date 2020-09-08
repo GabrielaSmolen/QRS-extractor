347c4ca274534b8343c0e9c426ecfede17950df2
GabrielaSmolen <gabrysiasmolen@gmail.com>
2020-09-08 13:28:39 +0200

    function for plotting single qrs

---
+++ plot_single_qrs.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def plot_single_qrs(path):
+
+    data = pd.read_csv(path, header=None)
+    values = data.values
+
+    for i in range(0, len(values)):
+        qrs = values[i, :]
+        if 'abnormal' in path:
+            plt.plot(qrs, color='r')
+            plt.show()
+        else:
+            plt.plot(qrs)
+            plt.show()
+
+
+if __name__ == '__main__':
+
+    path = 'data/mit-bih-arrhythmia-database-1.0.0/100_abnormal.csv'
+    plot_single_qrs(path)
