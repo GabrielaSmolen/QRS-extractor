149542f3214e65b5a24f0b318c05215e561cf049
GabrielaSmolen <gabrysiasmolen@gmail.com>
2020-09-18 15:42:45 +0200

    Minor changes

--- plot_single_qrs.py
+++ plot_single_qrs.py
@@ -19,5 +19,5 @@ def plot_single_qrs(path):
 
 if __name__ == '__main__':
 
-    path = 'data/mit-bih-arrhythmia-database-1.0.0/232_normal.csv'
+    path = '../data/mit-bih-arrhythmia-database-1.0.0/232_normal.csv'
     plot_single_qrs(path)
