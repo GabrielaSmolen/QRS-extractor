ae2d911ab330beb026a520af2178b59f93c69c81
Gabriela <gabrysiasmolen@gmail.com>
2020-09-24 16:26:42 +0200

    Function modification

--- ecg_analysis.py
+++ ecg_analysis.py
@@ -5,14 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def qrs_analysis(signal):
-    detected_qrs_samples_indexes = np.array(detect_qrs(signal, cutoff=20, fs=fs, order=3))
-    detected_qrs_samples = samples[detected_qrs_samples_indexes[:-1]]
+def qrs_analysis(samples: np.ndarray, fs: int):
+    detected_qrs_samples_indices = np.array(detect_qrs(samples, cutoff=20, fs=fs, order=3))
+    detected_qrs_samples = samples[detected_qrs_samples_indices[:-1]]
     detected_qrs_samples = detected_qrs_samples.tolist()
     window_len = 150 * fs / 1000
     morphology_class = []
-    detected_qrs_samples_indexes = detected_qrs_samples_indexes[:-1].tolist()
-    for sample in detected_qrs_samples_indexes:
+    detected_qrs_samples_indices = detected_qrs_samples_indices[:-1].tolist()
+    for sample in detected_qrs_samples_indices:
         one_qrs = []
         for i in range(int(sample - window_len), int(sample + window_len)):
             one_qrs.append(i)
@@ -24,14 +24,14 @@ def qrs_analysis(signal):
     plt.figure()
     for i in range(0, len(morphology_class)):
         if morphology_class[i] == 'N':
-            plt.plot(detected_qrs_samples_indexes[i], detected_qrs_samples[i], color='g', marker='$N$', zorder=3)
+            plt.plot(detected_qrs_samples_indices[i], detected_qrs_samples[i], color='g', marker='$N$', zorder=3)
         else:
-            plt.plot(detected_qrs_samples_indexes[i], detected_qrs_samples[i], color='r', marker='$V$', zorder=3)
+            plt.plot(detected_qrs_samples_indices[i], detected_qrs_samples[i], color='r', marker='$V$', zorder=3)
 
     plt.plot(samples, zorder=-1)
     plt.show()
 
-    return morphology_class
+    return morphology_class, detected_qrs_samples_indices
 
 
 if __name__ == '__main__':
@@ -39,6 +39,6 @@ if __name__ == '__main__':
     samples = np.array(signal.p_signal[:, 0])
     fs = signal.fs
 
-    morphology_class = qrs_analysis(samples)
+    morphology, detected_indices = qrs_analysis(samples, fs)
 
     print()
